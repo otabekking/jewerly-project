f4dad29f2b5fb999e42be1c2d23a385bf598c05c
Otabek <otabekabdulazizov282@gmail.com>
2021-10-06 14:55:21 +0500

    jewerly gold

---
+++ java.js
@@ -0,0 +1,19 @@
+"use strict"
+let title = new Vue({
+    el: "#ring_title",
+    data: {
+        title: "Jewelery tells a great story"
+    }
+});
+let ring2 = new Vue({
+    el: "#story__title",
+    data: {
+        story: "Our story"
+    }
+});
+let text = new Vue({
+    el: "#text",
+    data: {
+        texts: "modern jewelry is made of gold, silver, or platinum, often with precious or semiprecious stones, it evolved from shells, animal teeth, and other items used as body decoration in prehistoric times."
+    }
+})
